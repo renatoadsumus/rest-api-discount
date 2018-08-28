ca78cf5cdd188466e1f499f156dc9adf7f35b64d
renatoadsumus <renatoadsumus@gmail.com>
2018-08-28 11:21:08 -0300

    Adding a integration test

--- test/integration/testApp.py
+++ test/integration/testApp.py
@@ -14,15 +14,15 @@ class TestApp(unittest.TestCase):
         self.assertEqual(result_actual, {'discount': result_expect})
         self.assertEqual(response.status_code, 200)
 
-        def test_app_purchase_total_is_30_and_discount_is_20(self):
-            tester = app.test_client(self)
-            response = tester.post('/', data=dict(post_total_purchase='30'))
+    def test_app_purchase_total_is_30_and_discount_is_20(self):
+        tester = app.test_client(self)
+        response = tester.post('/', data=dict(post_total_purchase='30'))
 
-            result_expect = [{'total_discount': 20}]
-            result_actual = json.loads(response.data.decode('utf-8'))
+        result_expect = [{'total_discount': 20}]
+        result_actual = json.loads(response.data.decode('utf-8'))
 
-            self.assertEqual(result_actual, {'discount': result_expect})
-            self.assertEqual(response.status_code, 200)
+        self.assertEqual(result_actual, {'discount': result_expect})
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
